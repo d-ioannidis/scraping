0ff6f81a97d6f73fe842037b2d2fd6e760151f51
dmitris1821gr <dimitrismagkas2@yahoo.gr>
2020-12-18 16:22:30 +0200

    create

---
+++ ProxyRotation/proxies.py
@@ -0,0 +1,85 @@
+import re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from random import choice as rchoice
+import requests
+
+class Proxy:
+    def __init__(self, user_agent, gpu, headless):
+        self.driver = None
+
+        self.address = []
+        self.port = []
+        self.new_address = []
+        self.new_port = []
+
+        self.chrome_options = Options()
+        self.chrome_options.add_argument(gpu)
+        self.chrome_options.add_argument(user_agent)
+        self.chrome_options.add_argument(headless)
+
+    def driver_start(self):
+        self.driver = webdriver.Chrome('chromedriver.exe', options=self.chrome_options)
+
+    def driver_quit(self):
+        self.driver.quit()
+
+    def driver_url(self, url):
+        self.driver.get(url)
+
+    def addresses(self, arg1):
+        content = self.driver.page_source
+        soup = BeautifulSoup(content, features='html.parser')
+
+        ip = soup.find_all(arg1)
+
+        for row in ip:
+            string = re.findall(r'[0-9].*[^</td>]', " " + row.get_text())
+            self.address.append(string)
+
+        self.address = self.address[::8]
+
+        del self.address[20::1]
+
+    def ports(self, arg1):
+        content = self.driver.page_source
+        soup = BeautifulSoup(content, features='html.parser')
+
+        ip = soup.find_all(arg1)
+
+        for row in ip:
+            string = re.findall(r'[0-9].*[^</td>]', " " + row.get_text())
+            self.port.append(string)
+
+        self.port = self.port[1::8]
+
+        del self.port[20::1]
+
+    def full_address(self):
+
+        for elem in self.address:
+            self.new_address.extend(elem)
+
+        for elem in self.port:
+            self.new_port.extend(elem)
+
+        for i, address in enumerate(self.new_address):
+            self.new_address[i] = self.new_address[i] + ":" + self.new_port[i]
+
+    def get_choice(self, request_method, url, **kwargs):
+        cho = []
+
+        while True:
+            try:
+                cho = rchoice(self.new_address)
+                choice = {'https': cho}
+                print('Proxy currently being tested: {}'.format(choice))
+                response = requests.request(request_method, url, proxies=choice, timeout=10, **kwargs)
+                if response.status_code == 200:
+                    print('Current proxy is functional.')
+                break
+            except:
+                print('Error: Could not connect, looking for another proxy.')
+                self.new_address.remove(cho)
+                pass
---
+++ ProxyRotation/rotation.py
@@ -0,0 +1,22 @@
+from ProxyRotation.proxies import Proxy
+
+USER_AGENT = "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+URL = "https://sslproxies.org/"
+GPU = "--disable-gpu"
+HEADLESS = "--headless"
+
+my_proxies = Proxy(USER_AGENT, GPU, HEADLESS)
+
+my_proxies.driver_start()
+my_proxies.driver_url(URL)
+
+arg1 = 'td'
+arg2 = ''
+arg3 = ''
+
+my_proxies.addresses(arg1)
+my_proxies.ports(arg1)
+my_proxies.full_address()
+my_proxies.get_choice(request_method='get', url=URL)
+
+my_proxies.driver_quit()
